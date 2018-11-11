fe16d8f41cce8355d0004cdf94d17fbdead9d7ab
Gideon <gideonb234@gmail.com>
2018-11-11 21:24:47 +0100

    update readme and index

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# youtube-cooking-timer
+# Youtube Cooking Timer
 
-> Time your cooking by watching youtube videos
+Time your cooking by having the first youtube video at your specific time show up
 
-## Build Setup
+## Setup this project on your computer
 
 ``` bash
 # install dependencies
@@ -17,5 +17,3 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="timer">
-    <p></p>
+    <p>Select a time</p>
+    <vue-timepicker v-on="findVideo" v-model="timeToPass" format="mm:ss"></vue-timepicker>
   </div>
 </template>
 
+<style>
+  .time-picker .dropdown .select-list .hours {
+    display: none;
+  }
+</style>
+
 <script>
+import VueTimepicker from 'vue2-timepicker'
+
 export default {
-  name: 'index'
+  name: 'index',
+  components: { VueTimepicker },
+  data () {
+    return {
+      timeToPass: {
+        mm: '10',
+        ss: '00'
+      }
+    }
+  },
+  findVideo: function (time) {
+    console.log(time)
+  }
 }
 </script>
