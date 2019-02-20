24f25ffbb150479584b3f4cfa23fd74840f142fc
Gideon <gideonb234@gmail.com>
2019-02-21 00:37:29 +0100

    update gitlab-ci.yml

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import app from './app'
-import router from './router'
+import app from '@/app'
+import router from '@/router'
 import VueYoutube from 'vue-youtube'
 
 Vue.config.productionTip = false
